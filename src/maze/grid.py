52400e1467c85700e2dd206062062f76010001bb
manu <manuel.lotito2005@gmail.com>
2025-07-17 16:53:36 -0300

    buttons clear, save, load

--- src/maze/grid.py
+++ src/maze/grid.py
@@ -1,68 +1,95 @@
-import os
+
 
 class Grid:
     SYMBOLS = {
-    "wall": "\u2B1B",
-    "path": "\u2B1C",
-    "start": "\u25B6",
-    "end": "\u23F9",
-    "visited": "\U0001F7E9"
+        "wall": "\u2B1B",
+        "path": "\u2B1C",
+        "start": "\u25B6",
+        "end": "\u23F9",
+        "visited": "\U0001F7E9"
+    }
+
+    PATHS = {
+        "main_matrix": "src\\texto.txt",
+        "user_matrix": "src\\texto1.txt"
     }
 
     def __init__(self, matrix, start, end):
         self.matrix = matrix
         self.start = start
-        self.end = end 
+        self.end = end
 
-    #Function to get matrix from the file 
+    # Function to get matrix from the file
     @classmethod
-    def from_file(cls, matrix_path):
+    def from_file(cls, matrix_path="src\\texto.txt"):
         open_path = open(matrix_path, "r")
         lines = open_path.readlines()
         open_path.close()
-    
+
         matrix = []
         start = None
         end = None
-        
+
         for y, line in enumerate(lines):
             row = []
-            
+
             for x, char in enumerate(line):
                 if char == "S":
-                    start = (x, y)
-                    row.append(" ")
+                    start = (y, x)
+                    row.append("S")
                 elif char == "E":
-                    end = (x, y)
-                    row.append(" ")
+                    end = (y, x)
+                    row.append("E")
                 elif char != "\n":
                     row.append(char)
 
             matrix.append(row)
 
         return cls(matrix, start, end)
-    
-    #Function to draw matrix
+
+    # Function to draw matrix
     def get_symbol(self, y, x):
-        if (x, y) == self.start:
+        if (y, x) == self.start:
             return self.SYMBOLS["start"]
-        elif (x, y) == self.end:
-            return self.SYMBOLS["end"] 
+        elif (y, x) == self.end:
+            return self.SYMBOLS["end"]
         elif self.matrix[y][x] == "#":
             return self.SYMBOLS["wall"]
         elif self.matrix[y][x] == " ":
             return self.SYMBOLS["path"]
 
-
-    #Function for cell changing
-    def change_cell(self, pos):
-        y, x = pos
+    # Function for cell changing
+    def change_cell(self, y, x):
 
         if self.matrix[y][x] not in ["S", "E"]:
+
             if self.matrix[y][x] == "#":
                 self.matrix[y][x] = " "
             else:
                 self.matrix[y][x] = "#"
 
+        return self.get_symbol(y, x)
 
+    # Function to clear the matrix
+    def clear_matrix(self):
+        for y in range(len(self.matrix)):
 
+            for x in range(len(self.matrix[0])):
+
+                if (y, x) not in [self.start, self.end]:
+                    self.matrix[y][x] = " "
+
+    # Function to save the matrix
+    def save_matrix(self):
+
+        path = self.PATHS["user_matrix"]
+
+        with open(path, "w") as my_maze:
+
+            for i in range(len(self.matrix)):
+
+                line = self.matrix[i]
+                my_maze.write("".join(line))
+
+                if i < len(self.matrix) - 1:
+                    my_maze.write("\n")
--- src/utils/visualizer.py
+++ src/utils/visualizer.py
@@ -1,84 +1,107 @@
 import tkinter as tk
 from tkinter import ttk
 from maze.grid import Grid
-from utils.functions import (change_cell, clear_matrix)
 
 
-
-class UI(tk.Frame): 
+class UI(tk.Frame):
 
     def __init__(self, parent=None):
         tk.Frame.__init__(self, parent)
         self.parent = parent
-        self.grid_object = Grid.from_file("src\\texto.txt")
+        self.grid_object = Grid.from_file()
+        self.grid_cell_array = []
         self.init_ui()
-
 
     def init_ui(self):
         self.parent.title("Maze Path Finder")
-        
-        #Control interface   
-        
-        #Sueprior Frame
+
+        # Control interface
+
+        # Sueprior Frame
         self.control_frame = tk.Frame(self.parent)
         self.control_frame.pack(pady=10)
 
-        #Clear Button
-        def clear_grid(array, matrix, start, end):
-            clear_matrix(matrix, start, end)
-            for cell in array:
-                cell.config(text="\u2B1C")
-        
-        clear_button = tk.Button(
-            self.control_frame, 
+        # Random Maze Button
+        tk.Button(
+            self.control_frame,
+            text="Random Maze",
+            ).pack(side=tk.LEFT, padx=(0, 20))
+
+        # Clear Button
+        tk.Button(
+            self.control_frame,
             text="Clear",
-            command= lambda m=self.grid_object.matrix, s=self.grid_object.start, e=self.grid_object.end: clear_grid(grid_cell_array, m, s, e)
+            command=self.clear_grid
             ).pack(side=tk.LEFT, padx=5)
-    
-        #Save button
-        save_button = tk.Button(
-            self.control_frame, 
-            text="Save"
+
+        # Save button
+        tk.Button(
+            self.control_frame,
+            text="Save",
+            command=lambda: self.grid_object.save_matrix()
             ).pack(side=tk.LEFT, padx=5)
-        
-        #Load button
-        load_button = tk.Button(
-            self.control_frame, 
+
+        # Load button
+        tk.Button(
+            self.control_frame,
             text="Load",
+            command=self.load_maze
             ).pack(side=tk.LEFT, padx=5)
-        
-        #Algorythms combox
-        Algorythms_list = ttk.Combobox(
+
+        # Algorythms combox
+        algorythms_list = ttk.Combobox(
             self.control_frame,
             state="readonly",
             values=["djkastra", "algorythm2", "algorythm3"]
         )
-        Algorythms_list.pack(side=tk.LEFT, padx=25)
-        Algorythms_list.set("Algorythms")
+        algorythms_list.pack(side=tk.LEFT, padx=(20, 0))
+        algorythms_list.set("Algorythms")
 
-        #Grid Frame
-        self.Grid_Frame = tk.Frame(self.parent)
-        self.Grid_Frame.pack(pady=10)
+        # Run Algorythm Button
+        tk.Button(
+            self.control_frame,
+            text="Run",
+            ).pack(side=tk.LEFT, padx=5)
 
-        def click_on_cell(cell, matrix, i, j):
-            change_cell(matrix, i, j)
-            if cell.cget("text") == "\u2B1B":
-                cell.config(text="\u2B1C")
-            else:
-                cell.config(text="\u2B1B")
-            print(matrix)
+        # Grid Frame
+        self.grid_frame = tk.Frame(self.parent)
+        self.grid_frame.pack(pady=10)
 
-        #Grid
-        grid_cell_array = []
+        self.create_visual_grid()
+
+    def click_on_cell(self, cell, i, j):
+        symbol = self.grid_object.change_cell(i, j)
+        cell.config(text=symbol)
+
+    def create_visual_grid(self):
+        self.grid_cell_array.clear()
         for i in range(len(self.grid_object.matrix)):
+
+            row = []
+
             for j in range(len(self.grid_object.matrix[0])):
                 cell = tk.Button(
-                    self.Grid_Frame,
+                    self.grid_frame,
                     text=self.grid_object.get_symbol(i, j),
                     width=2,
                     height=1,
                 )
                 cell.grid(row=i, column=j)
-                if (j, i) not in [self.grid_object.start, self.grid_object.end]:
-                    cell.config(command= lambda b=cell, i=i, j=j: click_on_cell(b, self.grid_object.matrix, i, j))
-                    grid_cell_array.append(cell)
+                if (i, j) not in [self.grid_object.start, self.grid_object.end]:
+                    cell.config(command=lambda c=cell, i=i, j=j: self.click_on_cell(c, i, j))
+                    row.append(cell)
+
+            self.grid_cell_array.append(row)
+
+    def load_maze(self):
+        self.grid_object = Grid.from_file(self.grid_object.PATHS["user_matrix"])
+        for widget in self.grid_frame.winfo_children():
+            widget.destroy()
+        self.create_visual_grid()
+
+    def clear_grid(self):
+        self.grid_object.clear_matrix()
+
+        for i, array in enumerate(self.grid_cell_array):
+            for j, cell in enumerate(array):
+                cell.config(text=self.grid_object.get_symbol(i, j))
